72fa2d9ae1e1f12b1d9245c4a6c46885eefd09c4
Honda Masahiro <hondams@gmail.com>
2025-04-07 23:56:28 +0900

    initial commit

---
+++ sample-batch/src/test/java/com/github/hondams/sample_parent/SampleParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hondams.sample_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sample-web/src/main/java/com/github/hondams/sample_parent/SampleParentApplication.java
@@ -0,0 +1,13 @@
+package com.github.hondams.sample_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleParentApplication.class, args);
+	}
+
+}
